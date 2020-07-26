24caeb352f775ec97e1d66d7ba5f4c16a3da49d5
retroKaizen <retrokaizen@gmail.com>
2020-07-26 22:56:54 +0545

    Added checkLineValidation function to Validation.cs

--- DrawWithCommandline/DrawWithCommandline/Validation.cs
+++ DrawWithCommandline/DrawWithCommandline/Validation.cs
@@ -50,7 +50,7 @@ namespace DrawWithCommandline
                 isSomethingInvalid = true;
             }
         }
-        
+
         public void checkLoopAndIfValidation()
         {
             int numberOfLines = txtCommand.Lines.Length;
@@ -140,6 +140,316 @@ namespace DrawWithCommandline
                     MessageBox.Show("IF Not Ended with 'ENDIF'");
                 }
             }
+        }
+
+        public void checkLineValidation(string lineOfCommand)
+        {
+            String[] keyword = { "circle", "rectangle", "triangle", "polygon", "drawto", "moveto", "repeat", "if", "endif", "loop", "endloop" };
+            String[] shapes = { "circle", "rectangle", "triangle", "polygon" };
+            String[] variable = { "radius", "width", "height", "counter", "size" };
+            lineOfCommand = Regex.Replace(lineOfCommand, @"\s+", " ");
+            string[] words = lineOfCommand.Split(' ');
+
+            //removing white spaces in between words
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Trim();
+            }
+            String firstWord = words[0].ToLower();
+            Boolean firstWordIsKeyword = keyword.Contains(firstWord);
+            if (firstWordIsKeyword)
+            {
+                Boolean firstWordIsShape = shapes.Contains(words[0].ToLower());
+                if (firstWordIsShape)
+                {
+                    if (words[0].ToLower().Equals("circle"))
+                    {
+                        if (words.Length == 2)
+                        {
+                            Boolean isInt = words[1].All(char.IsDigit);
+                            if (!isInt)
+                            {
+                                //if it is not a variable then invalid
+
+                                Boolean isVariable = variable.Contains(words[1].ToLower());
+                                if (isVariable)
+                                {
+                                    checkIfVariableDefined(words[1]);
+                                }
+                                else
+                                {
+                                    isValidCommand = false;
+                                }
+
+                                //throw new NonDigitValueException("The value is not numerical \r\n It is not an error but just showing custom made exception.");
+
+                            }
+                        }
+                        else
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else if (words[0].ToLower().Equals("rectangle"))
+                    {
+                        String args = lineOfCommand.Substring(9, (lineOfCommand.Length - 9));
+                        String[] parms = args.Split(',');
+
+                        if (parms.Length == 2)
+                        {
+                            Boolean isInt = false;
+                            for (int i = 0; i < parms.Length; i++)
+                            {
+                                parms[i] = parms[i].Trim();
+                                isInt = parms[i].All(char.IsDigit);
+                                if (!isInt)
+                                {
+                                    //if it is not a variable then invalid
+
+                                    Boolean isVariable = variable.Contains(parms[i].ToLower());
+                                    if (!isVariable)
+                                    {
+                                        isValidCommand = false;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else if (words[0].ToLower().Equals("triangle"))
+                    {
+                        String args = lineOfCommand.Substring(8, (lineOfCommand.Length - 8));
+                        String[] parms = args.Split(',');
+
+                        if (parms.Length == 3)
+                        {
+                            Boolean isInt = false;
+                            for (int i = 0; i < parms.Length; i++)
+                            {
+                                parms[i] = parms[i].Trim();
+                                isInt = parms[i].All(char.IsDigit);
+                                if (!isInt)
+                                {
+                                    isValidCommand = false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else if (words[0].ToLower().Equals("polygon"))
+                    {
+                        String args = lineOfCommand.Substring(7, (lineOfCommand.Length - 7));
+                        String[] parms = args.Split(',');
+
+                        if (parms.Length == 8 || parms.Length == 10)
+                        {
+                            Boolean isInt = false;
+                            for (int i = 0; i < parms.Length; i++)
+                            {
+                                parms[i] = parms[i].Trim();
+                                isInt = parms[i].All(char.IsDigit);
+                                if (!isInt)
+                                {
+                                    isValidCommand = false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else { }
+                }
+                else if (firstWord.Equals("loop"))
+                {
+                    if (words.Length == 2)
+                    {
+                        Boolean isInt = words[1].All(char.IsDigit);
+                        if (!isInt)
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                    }
+                }
+                else if (firstWord.Equals("end"))
+                {
+                    if (words.Length == 2)
+                    {
+                        if (!words[1].Equals("loop"))
+                        {
+                            isValidCommand = false;
+                        }
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                    }
+                }
+                else if (firstWord.Equals("repeat"))
+                {
+                    if (words.Length >= 4 && words.Length <= 6)
+                    {
+                        Boolean isInt = words[1].All(char.IsDigit);
+                        if (isInt)
+                        {
+                            if (shapes.Contains(words[2].ToLower()))
+                            {
+
+                                Boolean hasPlus = words[3].Contains('+');
+                                if (hasPlus)
+                                {
+                                    string[] words2 = words[3].Split('+');
+                                    for (int i = 0; i < words2.Length; i++)
+                                    {
+                                        words2[i] = words2[i].Trim();
+                                    }
+                                    Boolean firstWordIsVariable = variable.Contains(words2[0].ToLower());
+                                    if (!firstWordIsVariable)
+                                    {
+                                        isValidCommand = false;
+                                    }
+                                    else
+                                    {
+                                        if (words2.Length != 2)
+                                        {
+                                            isValidCommand = false;
+                                        }
+                                        else
+                                        {
+                                            //third char should be int to be valid
+
+                                            Boolean isInt2 = words2[1].All(char.IsDigit);
+                                            if (!isInt2)
+                                            {
+                                                isValidCommand = false;
+                                            }
+                                        }
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (variable.Contains(words[3].ToLower()))
+                                    {
+                                        if (words[4].Trim().Equals("+"))
+                                        {
+                                            Boolean isInt3 = words[5].All(char.IsDigit);
+                                            if (!isInt3)
+                                            {
+                                                isValidCommand = false;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Boolean hasPlus2 = words[4].Contains('+');
+                                            if (hasPlus2)
+                                            {
+                                                string[] words2 = words[4].Split('+');
+                                                for (int i = 0; i < words2.Length; i++)
+                                                {
+                                                    words2[i] = words2[i].Trim();
+                                                }
+                                                if (words2.Length == 2)
+                                                {
+                                                    Boolean isInt2 = words2[1].All(char.IsDigit);
+                                                    if (!isInt2)
+                                                    {
+                                                        isValidCommand = false;
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    isValidCommand = false;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                    }
+                }
+                else if (firstWord.Equals("if"))
+                {
+                    if (words.Length == 5)
+                    {
+                        if (variable.Contains(words[1].ToLower()))
+                        {
+                            if (words[2].Equals("="))
+                            {
+                                Boolean isInt = words[3].All(char.IsDigit);
+                                if (isInt)
+                                {
+                                    if (words[4].ToLower().Equals("then"))
+                                    {
+
+                                    }
+                                    else { isValidCommand = false; }
+                                }
+                                else { isValidCommand = false; }
+
+                            }
+                            else { isValidCommand = false; }
+                        }
+                        else { isValidCommand = false; }
+
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                    }
+
+                }
+                else if (firstWord.Equals("endif"))
+                {
+                    if (words.Length != 1)
+                    {
+                        isValidCommand = false;
+                    }
+                }
+                else if (firstWord.Equals("drawto") || firstWord.Equals("moveto"))
+                {
+                    String args = lineOfCommand.Substring(6, (lineOfCommand.Length - 6));
+                    String[] parms = args.Split(',');
+
+                    if (parms.Length == 2)
+                    {
+                        Boolean isInt = false;
+                        for (int i = 0; i < parms.Length; i++)
+                        {
+                            parms[i] = parms[i].Trim();
+                            isInt = parms[i].All(char.IsDigit);
+                            if (!isInt)
+                            {
+                                isValidCommand = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                    }
+                }
+            }
+            
+
         }
         
     }
